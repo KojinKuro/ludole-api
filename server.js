4e46244ef4986ef8500a0447a0fdaa4af9bc4e0b
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-05-24 16:14:45 -0500

    Add error handling for getting and posting a game.

--- server.js
+++ server.js
@@ -3,16 +3,33 @@ const PORT = process.env.PORT || 8000;
 const knex = require('./knex/knex.js');
 const app = express();
 
-app.use(express.json())
-app.get('/api/v1/game', (req, res) => {
- res.send("getting all games")
-}).post('/api/v1/game', (req, res)=> {
-    res.send(req.body)
+app.use(express.json());
+
+app.get('/api/v1/game', async (req, res) => {
+  try{
+    const game = await knex('game');
+    res.json(game)
+  }catch (err) {
+    res.status(500).json({message: "Error getting game!"})
+  }
+  // res.send("getting all games")
+})
+app.post('/api/v1/game', async (req, res)=> {
+  const {title, imagesrc, year, genre, themes,console,developer,publisher} = req.body;
+
+  if (!title || !imagesrc || !year || !genre ||!themes || !console || !developer || !publisher) {
+    return res.status(400).json({message:"Please add all relevant information!"})
+  }
+  
+  try {
+    const [id] = await knex('game').insert({title, imagesrc, year, genre, themes,console,developer,publisher}).returning('id');
+    res.status(201).json({id});
+  } catch (err) {
+    res.status(500).json({message: "Error adding game!"})
+  }
+  //res.send(req.body)
 })
 
-app.get('/tasks', (req, res) => {
- 
-});
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
