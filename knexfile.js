3825d3497c421341cc123347afc28ae5f2fc67a8
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-05-27 10:21:45 -0700

    Update to use database

--- knexfile.js
+++ knexfile.js
@@ -7,10 +7,28 @@ module.exports = {
   development: {
     client: "postgresql",
     connection: {
-      database: "lydiasims",
+      host: "127.0.0.1",
+      port: 5432,
+      database: "kojin",
       user: "postgres",
       password: "",
     },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: __dirname + "/knex/migrations",
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: __dirname + "/knex/seeds",
+      tableName: "knex_seeds",
+    },
+  },
+  production: {
+    client: "postgresql",
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
--- server.js
+++ server.js
@@ -1,55 +1,52 @@
 const express = require("express");
-const PORT = process.env.PORT || 8000;
 const knex = require("./knex/knex.js");
-const app = express();
 
+const PORT = process.env.PORT || 8000;
+const app = express();
 app.use(express.json());
 
-app.get("/api/v1/game", async (req, res) => {
+app
+  .get("/api/v1/game", async (req, res) => {
+    try {
+      const game = await knex("game");
+      res.json(game);
+    } catch (err) {
+      res.status(500).json({ message: "Error getting game!" });
+    }
+  })
+  .post("/api/v1/game", async (req, res) => {
+    if (!req.body) {
+      return res
+        .status(400)
+        .json({ message: "Please add all relevant information!" });
+    }
+
+    try {
+      const [id] = await knex("game")
+        .insert({
+          ...req.body,
+        })
+        .returning("id");
+      res.status(201).json({ id });
+    } catch (err) {
+      res.status(500).json({ message: "Error adding game!" });
+    }
+    //res.send(req.body)
+  });
+
+app.get("/api/v1/game/:id", async (req, res) => {
   try {
-    const game = await knex("game");
+    const { id } = req.params;
+    const game = await knex("game").where("id", parseInt(id));
+
+    if (!game.length) {
+      return res.json({ message: `Could not find game with ${id}` });
+    }
+
     res.json(game);
   } catch (err) {
     res.status(500).json({ message: "Error getting game!" });
   }
-  // res.send("getting all games")
-});
-app.post("/api/v1/game", async (req, res) => {
-  const {
-    title,
-    imagesrc,
-    year,
-    genre,
-    themes,
-    console,
-    developer,
-    publisher,
-  } = req.body;
-
-  if (!req.body) {
-    return res
-      .status(400)
-      .json({ message: "Please add all relevant information!" });
-  }
-
-  try {
-    const [id] = await knex("game")
-      .insert({
-        title,
-        imagesrc,
-        year,
-        genre,
-        themes,
-        console,
-        developer,
-        publisher,
-      })
-      .returning("id");
-    res.status(201).json({ id });
-  } catch (err) {
-    res.status(500).json({ message: "Error adding game!" });
-  }
-  //res.send(req.body)
 });
 
 app.listen(PORT, () => {
