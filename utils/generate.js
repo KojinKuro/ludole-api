480cd5224ba9a17ea413c1f919c8274c2283cc2b
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-06-17 01:20:07 -0700

    Remove unused code

--- utils/generate.js
+++
@@ -1,9 +0,0 @@
-import { createHash } from "crypto";
-
-// NOTE: this code can only be run in the server because crypto is a Node.js module.
-
-// can generate number between 0 to UPPER_RANGE - 1
-export function generateNumberFromSeed(KEY, UPPER_RANGE) {
-  const HASH = createHash("sha1").update(String(KEY)).digest().readUInt32BE();
-  return HASH % UPPER_RANGE;
-}
